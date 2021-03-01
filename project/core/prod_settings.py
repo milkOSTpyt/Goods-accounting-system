9ca5616a7a78ddb2af4b86448f5153688281114a
Artem <filiptema40@gmail.com>
2021-03-02 00:56:51 +0300

    apdute

---
+++ project/core/prod_settings.py
@@ -0,0 +1,13 @@
+
+SECRET_KEY = 'b#^di6u0zal6^2gr!aa3bje#z%5123!mya@)&3p-21d(=sj5%_y9n32122'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'gasdb',
+        'USER': 'artem',
+        'PASSWORD': 'gastest911',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
